4aef397c62f4f669943bc5687a6fd5d13c42fbc5
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2022-08-22 06:45:34 +0500

    pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+if(document.querySelector(".education__aside .item")) {
+    $(".education__aside .item").click(function() {
+        $(this).toggleClass("active");
+    });
+}
+
+if(document.querySelector(".education_3__block")) {
+    $(".education_3__block").click(function() {
+        $(this).toggleClass("open");
+    });
+}
